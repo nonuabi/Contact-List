8d9f36440cee2e76ef237a742c7f62e0f22332b9
nonuabi <nonuabi@gmail.com>
2021-04-17 12:32:56 +0530

    printing contact list using template engines with out using any db

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const path = require("path");
+const port = 8000;
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+var contactList = [
+  {
+    name: "Abhishek",
+    phone: "1111111111",
+  },
+  {
+    name: "Tony Stark",
+    phone: "2222222222",
+  },
+  {
+    name: "deepak",
+    phone: "3333333333",
+  },
+];
+
+app.get("/", function (req, res) {
+  return res.render("home", {
+    title: "Contact List!",
+    contact_list: contactList,
+  });
+});
+
+app.get("/practice", function (req, res) {
+  return res.render("practice", {
+    title: "Let us play with ejs",
+  });
+});
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log("error in running the server ", err);
+    return;
+  }
+  console.log("Yup! My Express Server is running on Port: ", port);
+});
